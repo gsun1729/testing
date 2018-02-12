a4974467a81606f4b4da8a0679dbfb8e519e14bb
Gordon Sun <gsun1729@gmail.com>
2018-02-12 12:44:53 -0500

    update main

--- main.py
+++ main.py
@@ -15,27 +15,57 @@ from sklearn.preprocessing import normalize
 from scipy import ndimage as ndi, stats
 from scipy.ndimage import gaussian_filter
 from skimage.feature import peak_local_max
-from skimage.filters import median, rank, threshold_otsu
-from skimage.morphology import disk, dilation, watershed, closing, skeletonize, medial_axis
+from skimage.filters import median, rank, threshold_otsu, laplace
+from skimage.morphology import (disk, dilation, watershed,
+								closing, opening, erosion, skeletonize, medial_axis)
 from skimage.segmentation import random_walker
-
-from scipy import fftpack
+from skimage.restoration import denoise_bilateral, estimate_sigma
+import scipy.signal as ss
 
 def main():
+	os.system('cls' if os.name == 'nt' else 'clear')
 	root = ".\\data\\generated"
 	# print get_img_filenames(root)
-	cell = io.imread(".\\data\\linhao\\hs\\P34A12_1_w1488 Laser.TIF")
-	mito = io.imread(".\\data\\linhao\\hs\\P34A12_1_w2561 Laser.TIF")
+	# Good Images
+	cell = io.imread(".\\data\\linhao\\hs\\P26H5_2_w1488 Laser.TIF")
+	mito = io.imread(".\\data\\linhao\\hs\\P26H5_2_w2561 Laser.TIF")
+	# Bad Images
+	# cell = io.imread(".\\data\\linhao\\hs\\P34A12_3_w1488 Laser.TIF")
+	# mito = io.imread(".\\data\\linhao\\hs\\P34A12_2_w2561 Laser.TIF")
+	sel_elem = disk(2)
+
 	a = max_projection(cell)
-	properties(a)
-	# a = median(a, disk(5))
+
+	b = smooth(a)
+	b1 = median(b,sel_elem)
+	c = erosion(b1,selem = disk(2))
+	d = dilation(c,selem = disk(2))
+
+	properties(c)
+	# montage_n_x(("a","b"),("b","a","c"),("c","d" ,"d" ,"a","b1"), ("a","c"))
+	montage_n_x((a,b,a,a,a,a),(b,a,c))
+	# montage_2x((a, b, b1, c, d), (a,b,c))
+
+	# q = fft_ifft(a, 175, pinhole = False)
+	# c = fft_ifft(a, 200, pinhole = False)
+	# z = fft_bandpass(a,r_range = (100,200),pinhole = False)
+	# sz = fft_bandpass(a,pinhole = True)
+	# montage_x((c,q,q-c,z,sz))
+	# print save_file(root, root, root, root)
+	# sigma_est = estimate_sigma(a, multichannel=False, average_sigmas=True)
+	# denoise_bilateral(a,sigma_color=0.1, sigma_spatial=15,
+    #             multichannel=False)
+	# print sigma_est
+
+	# z = median(a, disk(5))
+	# montage_x((a,z))
 	# a = gaussian_filter(a,disk(1.5), mode='constant')
-	a = gamma_stabilize(a)
-	
-	q = robust_binarize(a)
+	# a = gamma_stabilize(a)
+
+	# q = robust_binarize(a)
 	# view_2d_img(disk_hole(mito[5,:,:],radius = 50, pinhole = True))
-	view_2d_img(q)
-	properties(q)
+	# view_2d_img(q)
+	# properties(q)
 
 
 	# q = median_layers(cell)
