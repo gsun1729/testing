68bdaa7cf17feab238b6c41dac455863f92a5e52
Gordon Sun <gsun1729@gmail.com>
2018-02-26 21:21:26 -0500

    attempted merge of mc_analyzer

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 
 import sys
+import os, errno
+import argparse
 sys.path.insert(0, '.\\lib')
 sys.path.insert(0, '.\\lines')
-import os, errno
-import cell_line
-import mito_line
-from render import *
-from read_write import *
-from skimage import io
-from processing import *
-import time, string, uuid
-import argparse
 
+import scipy.io
+from skimage import io
+
+from lib.UUID import *
+from lib.render import *
+from lib.read_write import *
+from lib.processing import *
+
+import lines.cell_line as cell_line
+import lines.mito_line as mito_line
+
+import time, string
 
 def blockPrint():
 	sys.stdout = open(os.devnull, 'w')
@@ -34,20 +39,10 @@ def main(args):
 
 
 	options = get_args(args)
-	# if len(options) != 4:
-	# 	print "> Wrong # of arguments, please provide input in the form \n> python main.py read_directory write_directory\n"
-	# 	for num, item in enumerate(options):
-	# 		print "> \tArg{}: {}\n".format(num, item)
-	# 	sys.exit()
-	# else:
-	print options
+
 	root_read_dir = options['read_dir']
 	save_dir = options['save_dir']
-	# save_dir_CELL = options[-2]
-	# save_dir_MITO = options[-1]
-	# 	print "> Parent Read Directory : {}\n".format(root_read_dir)
-	# 	print "> CELL Save Directory : {}\n".format(save_dir_CELL)
-	# 	print "> MITO Save Directory : {}\n".format(save_dir_MITO)
+
 	print "> Parent Read Directory : {}\n".format(root_read_dir)
 	print "> Save Directory : {}\n".format(save_dir)
 
@@ -58,8 +53,6 @@ def main(args):
 	mkdir_check(save_dir_cell)
 	mkdir_check(save_dir_mito)
 	mkdir_check(save_dir_anal)
-
-
 
 	start = time.time()
 	filenames = get_img_filenames(root_read_dir)
@@ -74,7 +67,7 @@ def main(args):
 	print "> Processing IDs saved here: {}\n".format(save_dir)
 
 
-	file_list_ID = open(os.path.join(save_dir, "filename_list.txt"),'w')
+	file_list_ID = open(os.path.join(save_dir, "UUID_LUT.txt"),'w')
 	for UID, img_name, img_fname, path_diff, img_loc, img_path in filenames:
 		file_list_ID.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format(UID, img_name, img_fname, path_diff, img_loc, img_path))
 	file_list_ID.close()
@@ -116,23 +109,35 @@ def main(args):
 	save_data(mito_stats, "mito_processing_RT", save_dir)
 	save_data(cell_stats, "cell_processing_RT",  save_dir)
 
-	# cell = ".\\data\\hs\\P11B3_2_w1488 Laser.TIF"
-	# mito = ".\\data\\hs\\P11B3_2_w2561 Laser.TIF"
-	# cell2 = ".\\data\\_hs\\P45F12_3_w1488 Laser.TIF"
-	# mito2 = ".\\data\\_hs\\P45F12_3_w2561 Laser.TIF"
-	# cell3 = ".\\data\\hs\\P26G1_1_w1488 Laser.TIF"
-	# mito3 = ".\\data\\hs\\P26G1_1_w2561 Laser.TIF"
-	# cell4 = ".\\data\\_hs\\P42A12_1_w1488 Laser.TIF"
-	# mito4 = ".\\data\\_hs\\P42A12_1_w2561 Laser.TIF"
-	# # # Bad Images
-	# # cellb = ".\\data\\hs\\P34A12_3_w1488 Laser.TIF"
-	# # mitob = ".\\data\\hs\\P34A12_2_w2561 Laser.TIF"
-	#
-	# cell_line.analyze(cell3)
-	# # binary_map = mito_line.analyze(mito)
-	# # skeleton3d = skeletonize_3d(binary_map)
-	# # stack_viewer(skeleton3d)
-	# sys.exit()
+
+	# Start merge of MC_analyzer
+	cell_filelist = get_img_filenames(save_dir_cell)
+	mito_filelist = get_img_filenames(save_dir_mito)
+	UUID_datatable = read_UUID_file(os.path.join(save_dir, "UUID_LUT.txt"))
+
+	C_M_UUID_pairs = create_pairTable(cell_filelist, UUID_datatable)
+	UUID_pairs = create_densepairTable(cell_filelist, UUID_datatable)
+
+	filename_pairs = []
+	for cell_UUID, mito_UUID in UUID_pairs:
+		filename_pairs.append(["C_" + cell_UUID + "_dat.mat",
+								"M_" + mito_UUID + "_bin.mat",
+								"M_" + mito_UUID + "_skel.mat"])
+
+	write_list_txt(save_dir_anal, "Cell_mito_UUID_Pairs.txt", C_M_UUID_pairs)
+	write_list_txt(save_dir_anal, "UUID_paired_filenames.txt", filename_pairs)
+
+	for filenames in filename_pairs:
+		save_fileID = get_UUID(filenames[0])
+		cell_img = scipy.io.loadmat(os.path.join(save_dir_cell, filenames[0]))['data']
+		mito_stack = scipy.io.loadmat(os.path.join(save_dir_mito, filenames[1]))['data']
+		mito_skel = scipy.io.loadmat(os.path.join(save_dir_mito, filenames[2]))['data']
+
+		labeled_mito_bin = stack_multiplier(cell_img, mito_stack)
+		labeled_mito_skel = stack_multiplier(cell_img, mito_skel)
+
+		save_data(labeled_mito_bin, "CM_" + save_fileID + "_bin", save_dir_anal)
+		save_data(labeled_mito_skel, "CM_" + save_fileID + "_skel", save_dir_anal)
 
 if __name__ == "__main__":
 	main(sys.argv)
