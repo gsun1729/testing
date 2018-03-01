7752d74f3d9e4bba8581faf639b4b285891e9ddd
Gordon Sun <gsun1729@gmail.com>
2018-02-28 21:31:18 -0500

    messing with noise

--- dev/tripleD.py
+++ dev/tripleD.py
@@ -1,6 +1,10 @@
 import sys
 from skimage import io
 import argparse
+from lib.render import *
+from lib.processing import *
+from skimage.morphology import disk
+# from lread_write import *
 
 def get_args(args):
 	parser = argparse.ArgumentParser(description = 'Script for analyzing 3d Images without 2d compression')
@@ -11,15 +15,33 @@ def get_args(args):
 	parser.add_argument('-w',
 						dest = 'write_path',
 						help = 'Results save directory',
-						required = True)
+						required = False)
 	options = vars(parser.parse_args())
 	return options
 
 
 def main(args):
 	options = get_args(args)
-	read_dir = options['read_path']
-	save_dir = options['write_path']
+	read_path = options['read_path']
+	save_path = options['write_path']
+
+	image = io.imread(read_path)
+	z_dim, x_dim, y_dim = image.shape
+	for slice_index in xrange(z_dim):
+		slice_data = image[slice_index, :, :]
+		noise_disk = disk(1)
+		output1 = gamma_stabilize(slice_data, alpha_clean = 0.05)
+		output2 = smooth_tripleD(output1, smoothing_px = 0.5, stdevs = 1.5)
+		output3 = dilation(output2, disk(3))
+		output3 = erosion(output3, disk(3))
+		output4 = median(output3, noise_disk)
+		output5 = img_type_2uint8(output4, func = 'floor')
+		output6 = binarize_image(output5, _dilation = 0, feature_size = 25)
+		output7 = improved_watershed(output6, output5, expected_separation = 20)
+		# asdf = fft_ifft(output2, FFT_filter)
+		montage_n_x((slice_data, output1, output2),( output3, output4, output5, output6, output7))
+	# stack_viewer(image)
+
 
 
 
