e29de21331dc937006321df410b8b5d2fa1fd186
Gordon Sun <gsun1729@gmail.com>
2018-02-28 21:32:19 -0500

    formatting

--- main.py
+++ main.py
@@ -28,8 +28,16 @@ def enablePrint():
 
 def get_args(args):
 	parser = argparse.ArgumentParser(description = 'Script for analyzing mitochondria skeletonization')
-	parser.add_argument('-r', dest = 'read_dir', help = 'Raw data read directory', required = False, default = ".\\data\\hs")
-	parser.add_argument('-w', dest = 'save_dir', help = 'Save directory for segmentation and skeletonization data', required = False, default = ".\\ooga")
+	parser.add_argument('-r',
+						dest = 'read_dir',
+						help = 'Raw data read directory',
+						required = False, 
+						default = ".\\data\\hs")
+	parser.add_argument('-w',
+						dest = 'save_dir',
+						help = 'Save directory for segmentation and skeletonization data',
+						required = False,
+						default = ".\\ooga")
 
 	options = vars(parser.parse_args())
 	return options
