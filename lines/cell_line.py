5bd139113b11a2f761e2c6d3e9fb80289c9842c9
gsun1729 <gsun1729@gmail.com>
2018-10-04 21:40:05 -0400

    last working cell line since last working model

--- lines/cell_line.py
+++ lines/cell_line.py
@@ -9,25 +9,26 @@ from skimage import io
 from skimage import measure
 from scipy.ndimage.morphology import binary_fill_holes, binary_opening
 from skimage.morphology import (disk, dilation, watershed,
-								closing, opening, erosion, skeletonize, medial_axis)
+								closing, opening, erosion, medial_axis)
 import matplotlib.pyplot as plt
+from skimage.filters import threshold_local
 cell_prefix = "C_"
 data_suffix = "_dat"
 projection_suffix = "_avgP"
 
 def get_args(args):
-	parser = argparse.ArgumentParser(description = 'Script for 3d segmenting mitochondria')
+	parser = argparse.ArgumentParser(description = 'Script for 3d segmenting cells')
 	parser.add_argument('-id',
 						dest = 'UUID',
 						help = 'Unique identifier (user generated)',
 						required = True)
 	parser.add_argument('-r',
 						dest = 'read_path',
-						help = 'read directory for data'
+						help = 'read directory for data',
 						required = True)
 	parser.add_argument('-w',
 						dest = 'write_path',
-						help = 'write directory for results'
+						help = 'write directory for results',
 						required = True)
 
 	options = vars(parser.parse_args())
@@ -35,33 +36,43 @@ def get_args(args):
 
 
 def analyze(UID, read_path, write_path):
-	try:
-		options = get_args(sys.argv)
-		read_path = options['read_path']
-		write_path = options['write_path']
-		UID = options['UUID']
-	except:
-		sys.exit()
-
 	cell = io.imread(read_path)
-
-	a1 = avg_projection(cell)
-	a2 = gamma_stabilize(a1,
-							alpha_clean = 1.3)
-	# Remove noise
-	a3 = smooth(a2,
-					smoothing_px = 0.5,
-					threshold = 1)
 	noise_disk = disk(1)
-	d1 = img_type_2uint8(a3, func = 'floor')
-	d2 = median(d1, noise_disk)
-	d3 = erosion(d2, noise_disk)
-	d4 = median(d3, noise_disk)
-	# Simple Thresholding
-	mean, stdev = px_stats(d4)
-	d4[d4 < mean + stdev] = 0
-	d5 = img_type_2uint8(d4, func = 'floor')
+	noise_processing = avg_projection(cell)
+	noise_processing = gamma_stabilize(noise_processing, alpha_clean = 1.3)
+	for i in xrange(1):
+		# Remove noise
+		noise_processing = smooth(noise_processing,
+									smoothing_px = 0.5,
+									threshold = 1)
 
+	noise_processing = img_type_2uint8(noise_processing, func = 'floor')
+	noise_processing = erosion(noise_processing, noise_disk)
+	noise_processing = median(noise_processing, noise_disk)
+
+	# d4 = median(d3, noise_disk)
+	# Simple Thresholding
+	d14 = smooth(noise_processing, smoothing_px = 4, threshold = 1)
+	test = threshold_local(noise_processing,
+									block_size = 31,
+									offset = 0)
+	# mean, stdev = px_stats(noise_processing)
+	# noise_processing[noise_processing < mean + stdev] = 0
+	# d5 = img_type_2uint8(noise_processing, func = 'floor')
+
+	temp_binary = d14 > test *0.9
+	view_2d_img(temp_binary)
+	wow = improved_watershed(temp_binary, d14,
+							expected_separation = 3)
+	wow2 = rm_eccentric(wow,
+						min_eccentricity = 0.7,
+						max_area = 2500)
+	# wow = label_and_correct(temp_binary, noise_processing, min_px_radius = 7,
+	# 						max_px_radius = 200,
+	# 						min_intensity = 0,
+	# 						mean_diff = 10)
+	montage_n_x((d14,noise_processing,  temp_binary, wow, wow2))
+	raise Exception
 	d6 = binarize_image(d5,
 							_dilation = 0,
 							feature_size = 25)
@@ -80,7 +91,8 @@ def analyze(UID, read_path, write_path):
 	d13 = rm_eccentric(d12,
 						min_eccentricity = 0.68,
 						max_area = 2500)
-	# montage_n_x((d12,d13))
+	montage_n_x((a1,d12,d13))
+	raise Exception
 	write_stats(d12, d13, UID,
 					"single_cell_stats.txt",
 					read_path,
@@ -92,4 +104,11 @@ def analyze(UID, read_path, write_path):
 
 
 if __name__ == "__main__":
-	analyze(sys.argv)
+		try:
+			options = get_args(sys.argv)
+			read_path = options['read_path']
+			write_path = options['write_path']
+			UID = options['UUID']
+			analyze(sys.argv)
+		except:
+			sys.exit()
