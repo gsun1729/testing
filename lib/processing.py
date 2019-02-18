801c742b853ec9a56f4d0bccb352664d3824a83a
gsun1729 <gsun1729@gmail.com>
2019-02-18 18:45:11 -0500

    transferred point functions to processing

--- lib/processing.py
+++ lib/processing.py
@@ -20,7 +20,6 @@ from scipy.ndimage.morphology import binary_fill_holes
 from skimage.segmentation import active_contour
 from skimage import measure
 
-from lib.point import *
 from lib.math_funcs import *
 from lib.render import *
 
@@ -720,4 +719,34 @@ def properties(image):
 	print("Global Max: {}\tGlobal Min: {}".format(global_max(image), global_min(image)))
 
 
-	
+def euclid_dist_nD(p0, p1):
+	return np.sum((p1 - p0) ** 2) ** 0.5
+
+
+class Point_set2D(object):
+	def __init__(self, point_list):
+		self.point_list = np.array([[float(coordinate) for coordinate in point] for point in point_list])
+
+
+	def num_pts(self):
+		return len(self.point_list)
+
+
+	def center_mass(self):
+		return np.sum(self.point_list, 0) / self.num_pts()
+
+
+	def perimeter(self):
+		peri_distance = 0
+		for pt_indx in xrange(self.num_pts()):
+			peri_distance += euclid_dist_nD(self.point_list[pt_indx],
+											self.point_list[pt_indx - 1])
+		return peri_distance
+
+
+	def shoelace(self):
+		area = 0
+		for pt in xrange(len(self.point_list)):
+			area += self.point_list[pt - 1][0] * self.point_list[pt][1]
+			area -= self.point_list[pt - 1][1] * self.point_list[pt][0]
+		return abs(area) / 2.0
